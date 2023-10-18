9ec3cd449f7c856554f81c8e495ac3a71d14d4f4
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-18 16:42:22 +0000

    setup pages

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ get("/") do
 end
 
 get("/square/new") do
+  
+  
+end
+
+get("/square/results") do
+  @num_to_square = params.fetch("number").to_i
+  #@square = @num_to_square*@num_to_square
 end
 
 get("/square_root/new") do
