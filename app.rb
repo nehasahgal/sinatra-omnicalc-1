e8b075fdae4465a5b1ff6a0e5a7ebbee25f6a258
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-18 16:28:00 +0000

    homepage done

--- app.rb
+++ app.rb
@@ -2,8 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ 
+  erb(:homepage)
+end
+
+get("/square/new") do
+end
+
+get("/square_root/new") do
+end
+
+get("/payment/new") do
+end
+
+get("/random/new") do
 end
